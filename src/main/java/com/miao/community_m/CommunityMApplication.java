1e61095001586e07b11bbead9242a6e05e884fae
Miao_Jingli <13572039108@163.com>
2021-07-15 17:51:53 +0800

    add index and bootstrap

---
+++ src/main/java/com/miao/community_m/CommunityMApplication.java
@@ -0,0 +1,13 @@
+package com.miao.community_m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityMApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miao/community_m/CommunityMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miao.community_m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityMApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
